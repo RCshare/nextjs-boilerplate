6e91c45fafe678d5b43f62ae040f4dd8655841b1
RCshare <37223660+RCshare@users.noreply.github.com>
2024-02-08 23:10:15 +0100

    Update Sentry.js

---
+++ Sentry.js
@@ -0,0 +1,24 @@
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://10ccd191a3295803134853079761bdcb@o4506240525598720.ingest.sentry.io/4506713417711616",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById(“app”);
+const root = createRoot(container);
+root.render(<App />);
