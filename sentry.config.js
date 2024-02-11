bd005af68e3723aa64f93bb497306df9cfab0b65
RCshare <37223660+RCshare@users.noreply.github.com>
2024-02-11 21:51:31 +0100

    Update sentry.config.js

--- sentry.config.js
+++ sentry.config.js
@@ -19,6 +19,6 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
-const container = document.getElementById(“app”);
+const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App />);
