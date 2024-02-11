74a005a4d92b7d664bbd9b3367eee9acc2a1442c
RCshare <37223660+RCshare@users.noreply.github.com>
2024-02-11 21:16:20 +0100

    Update ClientInteractiveComponent.tsx
    Handle click

--- components/ClientInteractiveComponent.tsx
+++ components/ClientInteractiveComponent.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import * as Sentry from "@sentry/react";
 
 export default function ClientInteractiveComponent() {
-  const handleClick = () => {
-    console.log('Button clicked!');
-    // Handle the click event
-  };
+      
+    const handleClick = () => {
+        try {
+            // This function should be defined elsewhere and is expected to possibly throw an error.
+            aFunctionThatMightFail();
+        } catch (err) {
+            Sentry.captureException(err);
+        }
+        console.log('Button clicked!');
+        // Handle the click event
+    };
 
-  return (
-    <button
-  type="button"
-  onClick={() => {
-    throw new Error("Sentry Test Error");
-  }}
->
-  Break the world
-</button>
-
-  );
+    return (
+        <button type="button" onClick={handleClick}>
+            Break the world
+        </button>
+    );
 }
+
+
 
